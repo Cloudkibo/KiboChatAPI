4f316a2200238efa7ace6960491c847f970b7d0e
Sojharo <sojharo@live.com>
2016-12-10 02:42:36 +0500

    mongodb 1, passportjs 2

--- controllers/Default.js
+++ controllers/Default.js
@@ -6,8 +6,84 @@ var url = require('url');
 var Default = require('./DefaultService');
 
 
-module.exports.geographiesGeo_idMediaRecentGET = function geographiesGeo_idMediaRecentGET (req, res, next) {
-  Default.geographiesGeo_idMediaRecentGET(req.swagger.params, res, next);
+module.exports.filetransfersCheckpendingfilePOST = function filetransfersCheckpendingfilePOST (req, res, next) {
+  Default.filetransfersCheckpendingfilePOST(req.swagger.params, res, next);
+};
+
+module.exports.filetransfersConfirmdownloadPOST = function filetransfersConfirmdownloadPOST (req, res, next) {
+  Default.filetransfersConfirmdownloadPOST(req.swagger.params, res, next);
+};
+
+module.exports.filetransfersDownloadPOST = function filetransfersDownloadPOST (req, res, next) {
+  Default.filetransfersDownloadPOST(req.swagger.params, res, next);
+};
+
+module.exports.filetransfersUploadPOST = function filetransfersUploadPOST (req, res, next) {
+  Default.filetransfersUploadPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupchatFetchSingleChatPOST = function groupchatFetchSingleChatPOST (req, res, next) {
+  Default.groupchatFetchSingleChatPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupchatPOST = function groupchatPOST (req, res, next) {
+  Default.groupchatPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupchatstatusCheckStatusPOST = function groupchatstatusCheckStatusPOST (req, res, next) {
+  Default.groupchatstatusCheckStatusPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupchatstatusGET = function groupchatstatusGET (req, res, next) {
+  Default.groupchatstatusGET(req.swagger.params, res, next);
+};
+
+module.exports.groupchatstatusUpdateStatusPOST = function groupchatstatusUpdateStatusPOST (req, res, next) {
+  Default.groupchatstatusUpdateStatusPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupmessagingDownloadIconPOST = function groupmessagingDownloadIconPOST (req, res, next) {
+  Default.groupmessagingDownloadIconPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupmessagingPOST = function groupmessagingPOST (req, res, next) {
+  Default.groupmessagingPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupmessagingSpecificGroupPOST = function groupmessagingSpecificGroupPOST (req, res, next) {
+  Default.groupmessagingSpecificGroupPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupmessagingUploadIconPOST = function groupmessagingUploadIconPOST (req, res, next) {
+  Default.groupmessagingUploadIconPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupmessaginguserLeaveGroupPOST = function groupmessaginguserLeaveGroupPOST (req, res, next) {
+  Default.groupmessaginguserLeaveGroupPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupmessaginguserMygroupsGET = function groupmessaginguserMygroupsGET (req, res, next) {
+  Default.groupmessaginguserMygroupsGET(req.swagger.params, res, next);
+};
+
+module.exports.groupmessaginguserMygroupsmembersGET = function groupmessaginguserMygroupsmembersGET (req, res, next) {
+  Default.groupmessaginguserMygroupsmembersGET(req.swagger.params, res, next);
+};
+
+module.exports.groupmessaginguserMyspecificgroupsmembersPOST = function groupmessaginguserMyspecificgroupsmembersPOST (req, res, next) {
+  Default.groupmessaginguserMyspecificgroupsmembersPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupmessaginguserPOST = function groupmessaginguserPOST (req, res, next) {
+  Default.groupmessaginguserPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupmessaginguserRemoveFromGroupPOST = function groupmessaginguserRemoveFromGroupPOST (req, res, next) {
+  Default.groupmessaginguserRemoveFromGroupPOST(req.swagger.params, res, next);
+};
+
+module.exports.groupmessaginguserUpdateRolePOST = function groupmessaginguserUpdateRolePOST (req, res, next) {
+  Default.groupmessaginguserUpdateRolePOST(req.swagger.params, res, next);
 };
 
 module.exports.userchatAlluserchatPOST = function userchatAlluserchatPOST (req, res, next) {
@@ -22,6 +98,30 @@ module.exports.userchatPOST = function userchatPOST (req, res, next) {
   Default.userchatPOST(req.swagger.params, res, next);
 };
 
+module.exports.userchatPartialchatsyncPOST = function userchatPartialchatsyncPOST (req, res, next) {
+  Default.userchatPartialchatsyncPOST(req.swagger.params, res, next);
+};
+
 module.exports.userchatSave2POST = function userchatSave2POST (req, res, next) {
   Default.userchatSave2POST(req.swagger.params, res, next);
+};
+
+module.exports.userchatUpdateStatusPOST = function userchatUpdateStatusPOST (req, res, next) {
+  Default.userchatUpdateStatusPOST(req.swagger.params, res, next);
+};
+
+module.exports.usersLogPOST = function usersLogPOST (req, res, next) {
+  Default.usersLogPOST(req.swagger.params, res, next);
+};
+
+module.exports.usersNewuserPOST = function usersNewuserPOST (req, res, next) {
+  Default.usersNewuserPOST(req.swagger.params, res, next);
+};
+
+module.exports.usersSearchaccountsbyphonePOST = function usersSearchaccountsbyphonePOST (req, res, next) {
+  Default.usersSearchaccountsbyphonePOST(req.swagger.params, res, next);
+};
+
+module.exports.usersSetstatusmessagePOST = function usersSetstatusmessagePOST (req, res, next) {
+  Default.usersSetstatusmessagePOST(req.swagger.params, res, next);
 };
--- controllers/DefaultService.js
+++ controllers/DefaultService.js
@@ -1,14 +1,481 @@
 'use strict';
 
-exports.geographiesGeo_idMediaRecentGET = function(args, res, next) {
+exports.filetransfersCheckpendingfilePOST = function(args, res, next) {
   /**
    * parameters expected in the args:
-  * geoId (Integer)
-  * count (Integer)
-  * min_id (Integer)
+  * uniqueid (String)
   **/
-  // no response value expected for this operation
-  res.end();
+    var examples = {};
+  examples['application/json'] = {
+  "filepending" : {
+    "date" : "aeiou",
+    "path" : "aeiou",
+    "file_name" : "aeiou",
+    "file_type" : "aeiou",
+    "from" : "aeiou",
+    "to" : "aeiou",
+    "_id" : "aeiou",
+    "uniqueid" : "aeiou",
+    "file_size" : "aeiou"
+  }
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.filetransfersConfirmdownloadPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * uniqueid (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.filetransfersDownloadPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * uniqueid (String)
+  **/
+    var examples = {};
+  examples['application/json'] = "";
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.filetransfersUploadPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * to (String)
+  * from (String)
+  * filename (String)
+  * filesize (String)
+  * filetype (String)
+  * uniqueid (String)
+  * file (file)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupchatFetchSingleChatPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * unique_id (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "msg" : "aeiou",
+  "date" : "aeiou",
+  "unique_id" : "aeiou",
+  "from" : "aeiou",
+  "from_fullname" : "aeiou",
+  "group_unique_id" : "aeiou",
+  "type" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupchatPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * group_unique_id (String)
+  * from (String)
+  * type (String)
+  * msg (String)
+  * from_fullname (String)
+  * unique_id (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "msg" : "aeiou",
+  "date" : "aeiou",
+  "unique_id" : "aeiou",
+  "from" : "aeiou",
+  "from_fullname" : "aeiou",
+  "group_unique_id" : "aeiou",
+  "type" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupchatstatusCheckStatusPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * unique_ids (List)
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "chat_unique_id" : "aeiou",
+  "user_phone" : "aeiou",
+  "read_date" : "aeiou",
+  "msg_unique_id" : "aeiou",
+  "status" : "aeiou",
+  "delivered_date" : "aeiou"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupchatstatusGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "chat_unique_id" : "aeiou",
+  "user_phone" : "aeiou",
+  "read_date" : "aeiou",
+  "msg_unique_id" : {
+    "msg" : "aeiou",
+    "date" : "aeiou",
+    "unique_id" : "aeiou",
+    "from" : "aeiou",
+    "from_fullname" : "aeiou",
+    "group_unique_id" : {
+      "unique_id" : "aeiou",
+      "group_name" : "aeiou",
+      "group_icon" : "aeiou",
+      "date_creation" : "aeiou"
+    },
+    "type" : "aeiou"
+  },
+  "status" : "aeiou",
+  "delivered_date" : "aeiou"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupchatstatusUpdateStatusPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * chat_unique_id (String)
+  * status (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessagingDownloadIconPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * unique_id (String)
+  **/
+    var examples = {};
+  examples['application/json'] = "";
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessagingPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * group_name (String)
+  * unique_id (String)
+  * members (List)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "unique_id" : "aeiou",
+  "group_name" : "aeiou",
+  "group_icon" : "aeiou",
+  "date_creation" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessagingSpecificGroupPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * unique_id (String)
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "unique_id" : "aeiou",
+  "group_name" : "aeiou",
+  "group_icon" : "aeiou",
+  "date_creation" : "aeiou"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessagingUploadIconPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * unique_id (String)
+  * file (file)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessaginguserLeaveGroupPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * group_unique_id (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessaginguserMygroupsGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "member_phone" : "aeiou",
+  "isAdmin" : "aeiou",
+  "group_unique_id" : {
+    "unique_id" : "aeiou",
+    "group_name" : "aeiou",
+    "group_icon" : "aeiou",
+    "date_creation" : "aeiou"
+  },
+  "display_name" : "aeiou",
+  "date_join" : "aeiou",
+  "membership_status" : "aeiou",
+  "date_left" : "aeiou"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessaginguserMygroupsmembersGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "member_phone" : "aeiou",
+  "isAdmin" : "aeiou",
+  "group_unique_id" : {
+    "unique_id" : "aeiou",
+    "group_name" : "aeiou",
+    "group_icon" : "aeiou",
+    "date_creation" : "aeiou"
+  },
+  "display_name" : "aeiou",
+  "date_join" : "aeiou",
+  "membership_status" : "aeiou",
+  "date_left" : "aeiou"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessaginguserMyspecificgroupsmembersPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * unique_id (String)
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "member_phone" : "aeiou",
+  "isAdmin" : "aeiou",
+  "group_unique_id" : {
+    "unique_id" : "aeiou",
+    "group_name" : "aeiou",
+    "group_icon" : "aeiou",
+    "date_creation" : "aeiou"
+  },
+  "display_name" : "aeiou",
+  "date_join" : "aeiou",
+  "membership_status" : "aeiou",
+  "date_left" : "aeiou"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessaginguserPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * group_name (String)
+  * group_unique_id (String)
+  * members (List)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "msg" : "aeiou",
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessaginguserRemoveFromGroupPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * group_unique_id (String)
+  * phone (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.groupmessaginguserUpdateRolePOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * group_unique_id (String)
+  * member_phone (String)
+  * makeAdmin (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
 }
 
 exports.userchatAlluserchatPOST = function(args, res, next) {
@@ -114,6 +581,40 @@ exports.userchatPOST = function(args, res, next) {
   
 }
 
+exports.userchatPartialchatsyncPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * user1 (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "msg" : [ {
+    "msg" : "aeiou",
+    "date" : "aeiou",
+    "date_server_sent" : "aeiou",
+    "owneruser" : "aeiou",
+    "fromFullName" : "aeiou",
+    "type" : "aeiou",
+    "file_type" : "aeiou",
+    "from" : "aeiou",
+    "_id" : "aeiou",
+    "to" : "aeiou",
+    "uniqueid" : "aeiou",
+    "date_server_received" : "aeiou",
+    "status" : "aeiou"
+  } ],
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
 exports.userchatSave2POST = function(args, res, next) {
   /**
    * parameters expected in the args:
@@ -129,6 +630,114 @@ exports.userchatSave2POST = function(args, res, next) {
     var examples = {};
   examples['application/json'] = {
   "uniqueid" : "aeiou",
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.userchatUpdateStatusPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * uniqueid (String)
+  * status (String)
+  * sender (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "uniqueid" : "aeiou",
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.usersLogPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * data (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.usersNewuserPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * display_name (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "date" : "aeiou",
+  "phone" : "aeiou",
+  "country_prefix" : "aeiou",
+  "id" : "aeiou",
+  "_id" : "aeiou",
+  "display_name" : "aeiou",
+  "national_number" : "aeiou",
+  "picture" : "aeiou",
+  "status" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.usersSearchaccountsbyphonePOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * phonenumbers (List)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "available" : [ "aeiou" ],
+  "notAvailable" : [ "aeiou" ]
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.usersSetstatusmessagePOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * status (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "msg" : "aeiou",
   "status" : "aeiou"
 };
   if(Object.keys(examples).length > 0) {
